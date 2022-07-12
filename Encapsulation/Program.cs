f321a589e9f169ecf5cbc4079bc4f8c513698f0c
Vostro 14 <Vostro 14@DESKTOP-9U24DL8>
2022-07-12 10:18:21 +0200

    Ajoutez des fichiers projet.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+public class BankAccount
+{
+    private string incubent;
+    public double Balance;
+    private string currency;
+
+    public string Incubent
+    {
+        get { return incubent; }
+        set { incubent = value; }
+    }
+
+    public string Currency
+    {
+       get { return currency; }
+        set { currency = value; }
+
+    }
+
+
+
+    public BankAccount(string incubent, double balance,string currency)
+    {
+        Incubent = incubent; 
+        Balance = balance;
+        Currency = currency;
+
+    }
+
+    public static void Main()
+    {
+        BankAccount RakiaAccount = new BankAccount("Rakia",500,"Dollar");
+
+
+       // how tyo use accesor:
+       string IncubentAccount= RakiaAccount.incubent;
+
+        Console.WriteLine(RakiaAccount.Balance + " " + RakiaAccount.currency);
+
+
+    }
+
+
+
+
+
+
+
+}
